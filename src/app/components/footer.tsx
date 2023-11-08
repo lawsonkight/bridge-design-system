f3678d2f3652d61e46b990402242fab53d90c8c7
Lawson Kight <lawsonkight@users.noreply.github.com>
2023-11-08 14:24:11 -0800

    footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,36 +1,51 @@
+type Props = {
+  header: string
+  links: { label: string, url: string }[]
+}
+
+function FooterSection({ header, links }: Props) {
+
+  const listItems = links.map(link => 
+    <a key={link.label} href={link.url} className="text-gray-400 hover:text-gray-300">{link.label}</a>
+  )
+
+  return <section className="flex flex-col gap-2">
+    <header className="text-gray-300 my-1">{header}</header>
+    {listItems}
+  </section>
+
+}
+
 export default function Footer() {
 
-  return <footer className="flex flex-col md:flex-row w-full p-3 text-sm text-gray-400 text-right">
+  return <footer className="flex flex-wrap w-full p-3 text-sm text-right">
     
-    <div className="grow p-4">
+    <div className="p-4">
       <img src='synapse-logo.svg' />
     </div>
     
-    <div className="flex flex-row md:flex-row gap-10 p-4">
+    <div className="flex flex-wrap gap-10 p-4 grow justify-end">
 
-      <section className="flex flex-col gap-2">
-        <header className="text-gray-300 my-1">Functions</header>
-        <a href='#'>Swap</a>
-        <a href='#'>Bridge</a>
-        <a href='#'>Pools</a>
-        <a href='#'>Stake</a>
-      </section>
+      <FooterSection header='Functions' links={[
+        { label: 'Swap', url: '#', },
+        { label: 'Bridge', url: '#', },
+        { label: 'Pools', url: '#', },
+        { label: 'Stake', url: '#', },
+      ]}/>
+      
+      <FooterSection header='Developers' links={[
+        { label: 'Build on Synapse', url: '#', },
+        { label: 'Documentation', url: '#', },
+        { label: 'Github', url: '#', },
+        { label: 'Blog', url: '#', },
+      ]}/>
 
-      <section className="flex flex-col gap-2">
-        <header className="text-gray-300 py-1">Developers</header>
-        <a href='#'>Build on Synapse</a>
-        <a href='#'>Documentation</a>
-        <a href='#'>Github</a>
-        <a href='#'>Blog</a>
-      </section>
-
-      <section className="flex flex-col gap-2">
-        <header className="text-gray-300 my-1">Support</header>
-        <a href='#'>Discord</a>
-        <a href='#'>Twitter</a>
-        <a href='#'>Forum</a>
-        <a href='#'>Telegram</a>
-      </section>
+      <FooterSection header='Support' links={[
+        { label: 'Discord', url: '#', },
+        { label: 'Twitter', url: '#', },
+        { label: 'Forum', url: '#', },
+        { label: 'Telegram', url: '#', },
+      ]}/>
 
     </div>
 
